1c92d9c1b1574cdfc4eb01f75b1abe3b4f229c3f
iamthesaint <stephenie2@me.com>
2024-10-22 17:16:23 -0400

    pseudo-coded all tests and started mockdata

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,1 +1,6 @@
-// COMPONENT tests for the quiz component
+// component tests for the quiz component
+
+// tests
+// the quiz renders properly with the correct number of questions
+// buttons: the "start quiz" button is visible on pageload and is clickable, the numbered answer buttons are clickable and move to the next question, and the "take new quiz" button is visible after the last question is submitted
+// the final score is calculated correctly and displayed once the quiz is completed
--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,2 +1,5 @@
-//  END TO END tests for the tech quiz
+//  e2e tests for the tech quiz
+
+// tests
+// the user flow is correct: the user can start the quiz, answer questions, and see their final score
 
